b1f767f1c63e88bd3b7d87f089213f994178808d
Flashyyyyyy <64888736+Flashyyyyyy@users.noreply.github.com>
2020-06-13 00:14:47 +0300

    Add files via upload

---
+++ src/commons.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <windows.h>
+#include <iostream>
+#include <cstdint>
+#include <intrin.h>
+#include <winternl.h>
+#include <vector>
+#include <d3d11.h>
+
+#include "utils/memory.h"
+#include "utils/utils.h"
---
+++ src/globals.h
@@ -0,0 +1,4 @@
+#pragma once
+
+inline Vector2 scr_size;
+inline Matrix4x4 viewmatrix;
